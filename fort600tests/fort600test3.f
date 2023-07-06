2a3588277a419d8617a790a52bf9cc3282c2420b
LogiFoxy <58698374+LogiFoxy@users.noreply.github.com>
2023-07-06 13:43:39 +0200

    Add files via upload

---
+++ fort600tests/fort600test3.f
@@ -0,0 +1,19 @@
+integer g, x(g), varia, i
+logical temp
+data x/5,-3,*2/, temp/2/
+
+if (f .gt. 1) i = 1 + 2
+varia = .false.
+
+if ( i .eq. 5 ) then
+    if ( f .ne. 0 ) 100, 200, 300
+else
+    integer l, i, k
+    
+    do i = 1, l
+        k = k + 1
+    enddo
+endif
+
+stop
+end
